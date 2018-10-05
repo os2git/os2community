ede91c8f116ada6dec30b9fe2cb934267bf69a18
Jesper Kristensen <cableman@linuxdev.dk>
2018-10-05 13:52:29 +0200

    SUPPORT-1767: Updated modules
    * addressfield_staticmap
    * backup_migrate
    * xmlsitemap

--- sites/all/modules/contrib/addressfield_staticmap/addressfield-staticmap-google-map.tpl.php
+++ sites/all/modules/contrib/addressfield_staticmap/addressfield-staticmap-google-map.tpl.php
@@ -1,25 +1,41 @@
 <?php $size = explode('x', $settings['size']); ?>
 
-<div id="map_canvas" style="width: <?php print $size[0]; ?>px; height: <?php print $size[1]; ?>px;">
+<?php if (isset($settings['text_address']) && is_string($settings['text_address'])): ?>
+  <div><?php print $settings['text_address']; ?></div>
+<?php endif; ?>
+
+<div id="map_canvas<?php if(isset($settings['id'])) print '-'.$settings['id']; ?>" style="width: <?php print $size[0]; ?>px; height: <?php print $size[1]; ?>px;">
   <noscript><?php print $image; ?></noscript>
 </div>
 
 <script type="text/javascript">
+var staticMapGoogleMap<?php if(isset($settings['id'])) print '_'.$settings['id']; ?> = new function() {
   var address = '<?php print $address; ?>';
 
   var myOptions = {
-    zoom: <?php print $settings['zoom']; ?>,
-    mapTypeId: google.maps.MapTypeId.<?php print strtoupper($settings['maptype']); ?>
-    //disableDefaultUI: true,
-    //scrollwheel: false,
-    //draggable: false,
-    //disableDoubleClickZoom: true
+  <?php if ($settings['zoom'] !== 'auto'): ?>zoom: <?php print $settings['zoom']; ?>,<?php endif ?>
+    mapTypeId: google.maps.MapTypeId.<?php print strtoupper($settings['maptype']); ?>,
+    scale: <?php print $settings['scale']; ?>,
+    scrollwheel: <?php print $settings['scroll_lock'] ? 'false' : 'true'; ?>,
+    <?php if ($settings['scroll_lock']) print 'gestureHandling: "none",' ?>
+    zoomControl: <?php print $settings['scroll_lock'] ? 'false' : 'true'; ?>,
   }
-  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+  var map = new google.maps.Map(document.getElementById('map_canvas<?php if(isset($settings['id'])) print '-'.$settings['id']; ?>'), myOptions);
+
+  <?php if (isset($settings['info_window'])): ?>
+  // info window
+  var content = document.createElement('div');
+  content.className = 'info-window';
+  content.innerHTML = '<?php print $settings['text_address']; ?>';
+
+  var infowindow = new google.maps.InfoWindow({
+  content: content
+  });
+  <?php endif; ?>
 
   <?php foreach ($kml_paths as $kml_path): ?>
-    var ctaLayer = new google.maps.KmlLayer('<?php print $kml_path; ?>');
-    ctaLayer.setMap(map);
+  var ctaLayer = new google.maps.KmlLayer('<?php print $kml_path; ?>');
+  ctaLayer.setMap(map);
   <?php endforeach; ?>
 
   var geocoder = new google.maps.Geocoder();
@@ -28,10 +44,16 @@
     if (status == google.maps.GeocoderStatus.OK) {
       map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-        map: map,
+      map: map,
         position: results[0].geometry.location
-
       });
+      <?php if (isset($settings['info_window'])): ?>
+      // info window
+      google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map,marker);
+      });
+      <?php endif; ?>
     }
- });
+  });
+}
 </script>
