bccd5f55d8dc380c8ab6394b2cef6c4ab7d6f9d7
Mikkel Ricky <rimi@aarhus.dk>
2019-05-09 22:45:49 +0200

    SERV-234: Applied security updates

--- sites/all/modules/contrib/eu_cookie_compliance/theme/eu-cookie-compliance-popup-info.tpl.php
+++ sites/all/modules/contrib/eu_cookie_compliance/theme/eu-cookie-compliance-popup-info.tpl.php
@@ -2,8 +2,7 @@
 
 /**
  * @file
- * This is a template file for a pop-up prompting user to give their consent for
- * the website to set cookies.
+ * Template file for consent banner.
  *
  * When overriding this template it is important to note that jQuery will use
  * the following classes to assign actions to buttons:
@@ -27,7 +26,7 @@
  */
 ?>
 <div>
-  <div class ="popup-content info">
+  <div class="popup-content info">
     <div id="popup-text">
       <?php print $message ?>
       <?php if ($disagree_button) : ?>
