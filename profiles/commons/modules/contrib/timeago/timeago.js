5bdf0a175d8cafba538aa93e9389381c4a9cafb2
Martin Yde Granath <my@aarhus.dk>
2019-11-06 15:07:13 +0100

    SERV-285: Remove unsused profile

--- profiles/commons/modules/contrib/timeago/timeago.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  Drupal.behaviors.timeago = {
-    attach: function (context) {
-      $.extend($.timeago.settings, Drupal.settings.timeago);
-      $('abbr.timeago, span.timeago, time.timeago', context).timeago();
-    }
-  };
-})(jQuery);
